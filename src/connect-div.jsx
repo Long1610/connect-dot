739ced80395bb91da64b4d0d555f258d69665aae
BM Gary <bm.hcmc.gary@gmail.com>
2024-03-03 23:52:43 +0700

    update code

--- src/connect-div.jsx
+++ src/connect-div.jsx
@@ -1,7 +1,14 @@
-const arrleft = ["a", "b", "c", "d", "e", "f"];
+const arrleft = [
+  { text: "a", id: "a" },
+  { text: "b", id: "b" },
+  { text: "c", id: "c" },
+  { text: "d", id: "d" },
+  { text: "e", id: "e" },
+  { text: "f", id: "f" },
+];
 const arrright = [
-  { text: "g", sub: "g", id: "g" },
-  { text: "h", sub: "h", id: "h" },
+  { text: "g", sub: "g", id: "g", color: "red" },
+  { text: "h", sub: "h", id: "h", color: "red" },
   { text: "i", sub: "i", id: "i" },
 ];
 import { getElementPosition } from "./get-position";
@@ -14,8 +21,40 @@ const ConnectDiv = () => {
   const [newLineSource, setNewLineSource] = useState(null);
 
   const handleCircleClick = (circleId) => {
+    const line = { from: newLineSource, to: circleId };
+    const exclude = ["g", "h", "i"];
+    const idsLeft = arrleft.map((x) => x.id);
+
+    if (exclude.includes(circleId) && exclude.includes(newLineSource)) {
+      setNewLineSource(null);
+      setLines([]);
+      return;
+    }
+
+    if (idsLeft.includes(circleId) && idsLeft.includes(newLineSource)) {
+      setNewLineSource(null);
+      setLines([]);
+      return;
+    }
+
+    const isInclude = arrleft.map((x) => x.id).includes(circleId);
+    const newLines = lines.some(
+      (item) => item.from !== circleId && item.to !== circleId
+    );
+
+    if (lines.length > 0 && isInclude) {
+      setLines(newLines);
+    }
+
+    console.log(circleId);
+    console.log(newLineSource);
+
+    console.log(exclude.includes(circleId));
+
+    console.log(exclude.includes(newLineSource));
+
     if (newLineSource && circleId !== newLineSource) {
-      setLines((p) => [...p, { from: newLineSource, to: circleId }]);
+      setLines((p) => [...p, line]);
       setNewLineSource(null);
     } else if (!newLineSource) {
       setNewLineSource(circleId);
@@ -31,54 +70,58 @@ const ConnectDiv = () => {
           <div
             key={index}
             className="box-left"
-            onClick={() => handleCircleClick(x)}
+            onClick={() => handleCircleClick(x.id)}
           >
-            <div className="text">{x}</div>
-            <div className="dot" id={x}></div>
+            <div className="text">{x.text}</div>
+            <div className="dot" id={x.id}></div>
           </div>
         ))}
       </div>
-      {lines.map((line, idx) => {
-        const sourcePos = getElementPosition(line.from);
-        const targetPos = getElementPosition(line.to);
-        const length = Math.sqrt(
-          Math.pow(targetPos[0] - sourcePos[0], 2) +
-            Math.pow(targetPos[1] - sourcePos[1], 2)
-        );
-        const angle =
-          Math.atan2(targetPos[1] - sourcePos[1], targetPos[0] - sourcePos[0]) *
-          (180 / Math.PI);
+      {Array.isArray(lines) &&
+        lines?.map((line, idx) => {
+          const sourcePos = getElementPosition(line.from);
+          const targetPos = getElementPosition(line.to);
 
-        if (!sourcePos || !targetPos) {
-          console.error("Invalid source line, probably:", {
-            line,
-            sourcePos,
-            targetPos,
-          });
-          return null;
-        }
+          const x1 = targetPos[0] + 4;
+          const x2 = sourcePos[0] + 4;
+          const y1 = targetPos[1] + 4;
+          const y2 = sourcePos[1] + 4;
 
-        return (
-          <div
-            key={idx}
-            style={{
-              position: "absolute",
-              background: "red",
-              width: length + "px",
-              height: "2px",
-              transform: `rotate(${angle}deg)`,
-              left: sourcePos[0] + "px",
-              top: targetPos[1] + "px",
-            }}
-            onClick={(lineIdx) =>
-              setLines([
-                ...lines.slice(0, lineIdx),
-                ...lines.slice(lineIdx + 1),
-              ])
-            }
-          ></div>
-        );
-      })}
+          const length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+          const angle = Math.atan2(y1 - y2, x1 - x2) * (180 / Math.PI);
+
+          if (!sourcePos || !targetPos) {
+            console.error("Invalid source line, probably:", {
+              line,
+              sourcePos,
+              targetPos,
+            });
+            return null;
+          }
+          const xMid = (x1 + x2) / 2;
+          const yMid = (y1 + y2) / 2;
+
+          return (
+            <div
+              key={idx}
+              style={{
+                position: "absolute",
+                background: "red",
+                width: length + "px",
+                height: "2px",
+                transform: `rotate(${angle}deg)`,
+                left: xMid - length / 2 + "px",
+                top: yMid + "px",
+              }}
+              onClick={(lineIdx) =>
+                setLines([
+                  ...lines.slice(0, lineIdx),
+                  ...lines.slice(lineIdx + 1),
+                ])
+              }
+            ></div>
+          );
+        })}
       <div className="wrapper-text-right">
         {arrright.map((x, index) => (
           <div
--- src/get-position.js
+++ src/get-position.js
@@ -1,10 +1,6 @@
 export function getElementPosition(id) {
     const element = document.getElementById(id)
     var rect = element.getBoundingClientRect();
-    console.log([
-        rect.left + window.scrollX,
-        rect.top + window.scrollY
-    ])
     return [
         rect.left + window.scrollX,
         rect.top + window.scrollY
